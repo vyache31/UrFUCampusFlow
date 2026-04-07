afdc6d68d7efd3f8a763e87808da2c931f9f8cfe
Вячеслав <slavasemenov007@gmail.com>
2026-04-07 17:57:19 +0500

    chore(team): add to main.py team router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from api.v1.endpoints import user
+from api.v1.endpoints import user, team
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(team.router)
