d437fa6e638cfce6b9eeb09441b87c26709bb56f
Анна Попова <pam-1-ap@yandex.ru>
2026-04-27 17:02:29 +0500

    fix: move test comments data to separate file

--- frontend/src/data/index.ts
+++ frontend/src/data/index.ts
@@ -2,3 +2,4 @@ export { testCases, type Case } from './cases';
 export { testTeams, type Team } from './teams';
 export { testMeetings, weeklyMeetings, type Meeting } from './meetings';
 export { testReportData, type TeamReport, type ReportMember } from './report';
+export { testComments, type Comment } from './comments';
