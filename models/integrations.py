87b58de2ec482ab9e3e4d265278dd43643116ff3
ecstasybtw <ecstasybtwru@gmail.com>
2026-05-11 19:00:20 +0500

    feat: add repo for meetings and some new fields into model

--- models/integrations.py
+++ models/integrations.py
@@ -1,7 +1,8 @@
 from sqlalchemy import (
     String, ForeignKey, DateTime
 )
-from datetime import datetime
+from datetime import datetime, timezone as TZ, timedelta
+from typing import Optional
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from database import Base
 
@@ -29,11 +30,20 @@ class Meetings(Base):
     __tablename__ = 'meetings'
 
     id: Mapped[str] = mapped_column(String(36), primary_key=True)
-    team_case_history_id: Mapped[str] = mapped_column(ForeignKey('team_case_history.id'))
+    team_case_history_id: Mapped[Optional[str]] = mapped_column(ForeignKey('team_case_history.id'), nullable=True)  # TODO: убрать nullable как только team_case_history_id будет готов
     title: Mapped[str]
-    date_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    location: Mapped[Optional[str]] = mapped_column(nullable=True)
+    start_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    end_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     outlook_event_id: Mapped[str]
     event_link: Mapped[str]
-    notes: Mapped[str]
+    notes: Mapped[Optional[str]] = mapped_column(nullable=True)
+    timezone: Mapped[Optional[int]] = mapped_column(nullable=True)
+
+    @property
+    def tz(self) -> TZ:
+        if self.timezone is None:
+            return TZ(timedelta(hours=5))
+        return TZ(timedelta(hours=self.timezone))
 
     team_case_history = relationship('TeamCaseHistory', back_populates='meetings')
